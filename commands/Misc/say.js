05b1b339109fa3305631977cd0495dc780c597cb
unknown <uqie.vanmadiun@gmail.com>
2024-09-21 10:13:40 +0700

    added: say command

---
+++ commands/Misc/say.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js")
+
+module.exports = {
+    name: "say",
+    aliases: [],
+    description: "Use it so the bot can speak according to what you type",
+    category: "Misc",
+    run: async (client, message, args) => {
+        let word = args.join(" ")
+        if(!word) return message.channel.send(new Discord.MessageEmbed().setDescription("<a:no:954773357407113298> | Please provide a message to say.").setColor("RED"))
+
+        message.delete()
+        message.channel.send(word)    
+    }
+}
