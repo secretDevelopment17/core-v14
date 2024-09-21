40914108a72ad4ab134d9a28523438d8dcdd85c3
unknown <uqie.vanmadiun@gmail.com>
2024-09-21 11:08:04 +0700

    chore: maybe fix

--- commands/Misc/avatar.js
+++ commands/Misc/avatar.js
@@ -7,7 +7,7 @@ module.exports = {
     description: "View other people's profile photos in HD quality",
     category: "Misc",
     run: async (client, message, args) => {
-        let user = message.mentions.users.first() || message.guild.resolveMember(args[0]) || message.author;
+        let user = message.mentions.users.first() || client.users.cache.get(args[0]) || message.author;
         let image = user.avatarURL({ dynamic: true, size: 4096 });
 
         let EmbedAvatar = new Discord.EmbedBuilder()
