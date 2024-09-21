971a0d8a031e204ac7e73e2464910424266cfac6
unknown <uqie.vanmadiun@gmail.com>
2024-09-21 12:25:11 +0700

    fix: maybe this is fix embed

--- commands/Misc/tag.js
+++ commands/Misc/tag.js
@@ -7,53 +7,107 @@ module.exports = {
     category: "Misc",
     run: async (client, message, args) => {
         if (!args[0]) {
-            message.channel.send(new Discord.EmbedBuilder().setDescription("<a:no:954773357407113298> | Not a valid command. I've \`add\`, \`show\`, \`list\`").setColor(Discord.Colors.Red))
+            message.channel.send({
+                embeds: [
+                    new Discord.EmbedBuilder()
+                        .setDescription("<a:no:954773357407113298> | Not a valid command. I've `add`, `show`, `list`")
+                        .setColor(Discord.Colors.Red)
+                ]
+            });            
           } else if (args[0] == "add") {
             if (!args[1]) {
-            message.channel.send(new Discord.EmbedBuilder().setDescription("<a:no:954773357407113298> | You need to specify a name to add new tag").setColor(Discord.Colors.Red))
+                message.channel.send({
+                    embeds: [
+                        new Discord.EmbedBuilder()
+                            .setDescription("<a:no:954773357407113298> | You need to specify a name to add a new tag")
+                            .setColor(Discord.Colors.Red)
+                    ]
+                });                
             } else {
               let name = args[1]
               let tag = await client.mongo.has(`tag`, name);
               let response = args.slice(2).join(" ");
               if (tag)
-                return message.channel.send(new Discord.EmbedBuilder().setDescription("<a:no:954773357407113298> | This tag name already exists").setColor(Discord.Colors.Red))
+                return message.channel.send({
+                    embeds: [
+                        new Discord.EmbedBuilder()
+                            .setDescription("<a:no:954773357407113298> | This tag name already exists")
+                            .setColor(Discord.Colors.Red)
+                    ]
+                });
+                
               if (!response)
-                return message.channel.send(new Discord.EmbedBuilder().setDescription("<a:no:954773357407113298> | You need to specify a response").setColor(Discord.Colors.Red))
+                return message.channel.send({
+                    embeds: [
+                        new Discord.EmbedBuilder()
+                            .setDescription("<a:no:954773357407113298> | You need to specify a response")
+                            .setColor(Discord.Colors.Red)
+                    ]
+                });
+                
         
               await client.mongo.set(`tag`, name, {
                 author: message.author.id,
                 name: name,
                 response: response,
               });
-
-              message.channel.send(
-                new Discord.EmbedBuilder()
-                  .setDescription(
-                    `<a:yes:954773528153059350> | Tag \`${name}\` has been added.`
-                  )
-                  .setColor("GREEN")
-              );
-              message.channel.send(new Discord.EmbedBuilder().setDescription("<a:yes:954773528153059350> | Tag \`${name}\` has been added.`").setColor(Discord.Colors.Green))
+              message.channel.send({
+                embeds: [
+                    new Discord.EmbedBuilder()
+                        .setDescription(`<a:yes:954773528153059350> | Tag \`${name}\` has been added.`)
+                        .setColor(Discord.Colors.Green)
+                ]
+            });            
             }
           } else if (args[0] == "delete") {
             if (!args[1]) {
-              message.channel.send(new Discord.EmbedBuilder().setDescription("<a:no:954773357407113298> | You need to specify a name to delete").setColor(Discord.Colors.Red))
+                message.channel.send({
+                    embeds: [
+                        new Discord.EmbedBuilder()
+                            .setDescription("<a:no:954773357407113298> | You need to specify a name to delete")
+                            .setColor(Discord.Colors.Red)
+                    ]
+                });                
             } else {
               let name = args[1]
               let tag = await client.mongo.has(`tag`, name);
               if (!tag)
-                return message.channel.send(new Discord.EmbedBuilder().setDescription("<a:no:954773357407113298> |This tag doesn't exist").setColor(Discord.Colors.Red))
+              return message.channel.send({
+                embeds: [
+                    new Discord.EmbedBuilder()
+                        .setDescription("<a:no:954773357407113298> | This tag doesn't exist")
+                        .setColor(Discord.Colors.Red)
+                ]
+            });            
               await client.mongo.delete(`tag`, name);
-              message.channel.send(new Discord.EmbedBuilder().setDescription(`<a:yes:954773528153059350> | Tag \`${name}\` has been deleted.`).setColor(Discord.Colors.Green))
+              message.channel.send({
+                embeds: [
+                    new Discord.EmbedBuilder()
+                        .setDescription(`<a:yes:954773528153059350> | Tag \`${name}\` has been deleted.`)
+                        .setColor(Discord.Colors.Green)
+                ]
+            });            
             }
           } else {
             if (!args[0]) {
-              message.channel.send(new Discord.EmbedBuilder().setDescription("<a:no:954773357407113298> | You need to specify a name to show").setColor(Discord.Colors.Red))
+                message.channel.send({
+                    embeds: [
+                        new Discord.EmbedBuilder()
+                            .setDescription("<a:no:954773357407113298> | You need to specify a name to show")
+                            .setColor(Discord.Colors.Red)
+                    ]
+                });                
             } else {
               let name = args[0];
               let tag = await client.mongo.get(`tag`, name);
               if (!tag)
-                return message.channel.send(new Discord.EmbedBuilder().setDescription("<a:no:954773357407113298> | This tag doesn't exist").setColor(Discord.Colors.Red))
+                return message.channel.send({
+                    embeds: [
+                        new Discord.EmbedBuilder()
+                            .setDescription("<a:no:954773357407113298> | This tag doesn't exist")
+                            .setColor(Discord.Colors.Red)
+                    ]
+                });
               message.channel.send(tag.response);
             }
           }
