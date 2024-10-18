8426dc299e4b9a376426b336b4177a2d23f3ffcb
unknown <uqie.vanmadiun@gmail.com>
2024-10-18 19:09:06 +0700

    fix(messageCreateEvent): fix anti link

--- index.js
+++ index.js
@@ -2,26 +2,8 @@ const discord = require("discord.js");
 const config = require("./config.json");
 const { KeyMongo } = require("key-mongo");
 const mongoose = require("mongoose");
-
-const lineReader = require('line-reader');
-const nvt = require('node-virustotal');
-const fs = require('fs');
-const path = require('path');
-const isMute = require("./database/Schema/isMute")
-const Case = require("./database/Schema/Case")
 require('./server.js');
 
-const filePath = path.join(__dirname, 'antilink.txt');
-let bannedUrls = [];
-
-fs.readFile(filePath, 'utf8', (err, data) => {
-  if (err) {
-      console.error('Error reading dangurls.txt:', err);
-      return;
-  }
-  bannedUrls = data.split(/\r?\n/).filter(url => url.trim() !== '');
-  console.log('Banned URLs loaded:', bannedUrls);
-});
 
 const client = new discord.Client({
     closeTimeout: 3_000 ,
@@ -78,46 +60,6 @@ process.on('uncaughtException', error => {
     client.logger.log("Uncaught Exception is detected, restarting...", "info");
     process.exit(1);
 });
-
-function containsBannedUrl(messageContent) {
-  const normalizedMessage = messageContent.toLowerCase(); // Normalisasi ke huruf kecil
-
-  // Mengecek setiap URL yang dilarang
-  for (const bannedUrl of bannedUrls) {
-      const normalizedBannedUrl = bannedUrl.toLowerCase();
-      
-      // Memeriksa jika URL ada dalam pesan
-      if (normalizedMessage.includes(normalizedBannedUrl)) {
-          return true; // Mengembalikan true jika ditemukan
-      }
-  }
-  return false; // Mengembalikan false jika tidak ditemukan
-}
-
-client.on("messageCreate", async (message) => {
-
-  if (containsBannedUrl(message.content)) {
-    try {
-        // Menghapus pesan jika mengandung URL yang terlarang
-        await message.delete();
-
-        // Mengirimkan embed peringatan ke channel
-        const alertEmbed = new discord.EmbedBuilder()
-            .setColor('#FF0000')
-            .setTitle('⚠ Malicious Link Detected ⚠')
-            .setDescription('A message containing a banned URL was detected and deleted.')
-            .setFooter({ text: 'The link sent may be malicious. Don\'t try to open it.' })
-            .setTimestamp();
-
-        await message.channel.send({ embeds: [alertEmbed] });
-
-        console.log(`Deleted a message containing a banned URL from ${message.author.tag}`);
-    } catch (err) {
-        console.error('Failed to delete message:', err);
-    }
-}
-
-})
 
 client.login(config.token).catch(() => { client.logger.log('Invaid TOKEN!', "warn") });
 
